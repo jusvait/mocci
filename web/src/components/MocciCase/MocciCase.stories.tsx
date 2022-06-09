b08bc7b2012983f305bf1f68ade3da993bde8abb
jusvait <42100008+jusvait@users.noreply.github.com>
2022-06-10 01:02:14 +0300

    Add tailwindcss

--- web/src/components/MocciCase/MocciCase.stories.tsx
+++ web/src/components/MocciCase/MocciCase.stories.tsx
@@ -1,7 +1,12 @@
 import MocciCase from './MocciCase'
 
 export const generated = () => {
-  return <MocciCase />
+  return <MocciCase mocciCase={{
+    id: 1,
+    name: 'Mocci Case',
+    createdAt: '2020-01-01',
+    participants: [{id: 1, name: 'Test guy'}]
+  }}/>
 }
 
 export default { title: 'Components/MocciCase' }
